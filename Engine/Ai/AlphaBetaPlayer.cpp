958d818f66c16c7e730973301ee3242b8e02eedc
Peter Fajdiga <peter.fajdiga@gmail.com>
2018-06-02 12:32:35 +0200

    estimate best depth according to number of pieces in game

--- Engine/Ai/AlphaBetaPlayer.cpp
+++ Engine/Ai/AlphaBetaPlayer.cpp
@@ -11,6 +11,30 @@ namespace reversi {
 
     AlphaBetaPlayer::~AlphaBetaPlayer() = default;
 
+
+    const double PIECES_GAUSS_A1 =   1.066;
+    const double PIECES_GAUSS_B1 =   18.29;
+    const double PIECES_GAUSS_C1 =   25.82;
+    const double PIECES_GAUSS_A2 = -0.5588;
+    const double PIECES_GAUSS_B2 =   6.164;
+    const double PIECES_GAUSS_C2 =    9.95;
+    double AlphaBetaPlayer::estimateTime(const size_t n_pieces) {
+        // pre-learned gauss function for depth 4
+        const double exp1 = (n_pieces-PIECES_GAUSS_B1)/PIECES_GAUSS_C1;
+        const double exp2 = (n_pieces-PIECES_GAUSS_B2)/PIECES_GAUSS_C2;
+        return PIECES_GAUSS_A1 * exp(-exp1 * exp1) +
+               PIECES_GAUSS_A2 * exp(-exp2 * exp2);
+    }
+
+    const size_t DEPTH_MIN = 4;
+    const size_t DEPTH_MAX = 10;
+    const size_t DEPTH_D = DEPTH_MAX-DEPTH_MIN;
+    size_t AlphaBetaPlayer::estimateDepth(const size_t n_pieces) {
+        const double time = estimateTime(n_pieces);
+        return (size_t)floor(DEPTH_MAX - time * DEPTH_D);
+    }
+
+
     const double weights[8][8] = {
             {16.160, -3.575,  1.160,  0.530,  0.530,  1.160, -3.575, 16.160},
             {-3.575, -1.810, -0.060, -0.225, -0.225, -0.060, -1.810, -3.575},
@@ -28,9 +52,11 @@ namespace reversi {
         double maxEvalScore = -INFINITY;
         Tile bestMove = legalMoves[0];  // default move if given state is not winnable
 
+        printf("pieces: %ld | depth: %ld\n", board.getPiecesCount(), estimateDepth(board.getPiecesCount()));
+
         for (const Tile& move : legalMoves) {
             Board child(board, move);
-            const double evalScore = negamax(child, 4, -INFINITY, INFINITY);
+            const double evalScore = negamax(child, estimateDepth(board.getPiecesCount()), -INFINITY, INFINITY);
             if (evalScore > maxEvalScore) {
                 maxEvalScore = evalScore;
                 bestMove = move;
@@ -40,7 +66,7 @@ namespace reversi {
     }
 
 
-    double AlphaBetaPlayer::negamax(const Board& board, int depth, double alpha, double beta) const {
+    double AlphaBetaPlayer::negamax(const Board& board, size_t depth, double alpha, double beta) const {
         if (depth == 0 || board.isGameOver()) {
             return evaluate(board);
         }
--- Engine/Ai/AlphaBetaPlayer.h
+++ Engine/Ai/AlphaBetaPlayer.h
@@ -13,9 +13,13 @@ namespace reversi {
 
         ~AlphaBetaPlayer() override;
 
+        static double estimateTime(const size_t n_pieces);
+
+        static size_t estimateDepth(const size_t n_pieces);
+
         Tile getMoveTimed(const Board& board) override;
 
-        double negamax(const Board& board, int depth, double alpha, double beta) const;
+        double negamax(const Board& board, size_t depth, double alpha, double beta) const;
 
         double evaluate(const Board& board) const;
     };

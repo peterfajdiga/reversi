d08f3a25d408e31a1e302e817dfe3c6c7dad3f68
Peter Fajdiga <peter.fajdiga@gmail.com>
2018-06-04 15:21:07 +0200

    MonteCarloTreePlayer keeps tree between turns

--- Engine/Ai/MonteCarloTreePlayer.cpp
+++ Engine/Ai/MonteCarloTreePlayer.cpp
@@ -6,94 +6,151 @@
 
 namespace reversi {
 
-    const double C = sqrt(2);
+    MCTree::MCTree(const Board& board) : parent(nullptr), board(board), wins(0), runs(0) {}
+    MCTree::MCTree(MCTree& parent, const Tile& move) : parent(&parent), board(parent.board, move), wins(0), runs(0) {}
+
+    void MCTree::expand() {
+        assert(children.empty());
+        for (const Tile& move : board.getLegalMoves()) {
+            children.emplace_back(new MCTree(*this, move));
+        }
+    }
+
+    const double C = 1.414213562373095;  // sqrt(2)
     const double EPS = 1e-14;
+    double MCTree::selectionFitness() const {
+        // UCT
+        return (double)wins / (runs+EPS) + C * sqrt(log(parent->runs+1) / (runs+EPS)) + rand() * EPS;
+    }
 
-    class Tree {
-    public:
-        Tree* const parent;
-        const Board board;
-        size_t wins, runs;
-        std::vector<Tree> children;
-
-        Tree(const Board& board) : parent(nullptr), board(board), wins(0), runs(0) {}
-        Tree(Tree& parent, const Tile& move) : parent(&parent), board(parent.board, move), wins(0), runs(0) {}
-
-        void expand() {
-            assert(children.empty());
-            for (const Tile& move : board.getLegalMoves()) {
-                children.emplace_back(Tree(*this, move));
-            }
-        }
-
-        double selectionFitness() const {
-            // UCT
-            return (double)wins / (runs+EPS) + C * sqrt(log(parent->runs+1) / (runs+EPS)) + rand() * EPS;
-        }
-
-        Tree& select() {
-            Tree* bestTree = this;
-            while (!bestTree->children.empty()) {
-                double maxFitness = -INFINITY;
-                for (Tree& tree : bestTree->children) {
-                    const double fitness = tree.selectionFitness();
-                    if (fitness > maxFitness) {
-                        maxFitness = fitness;
-                        bestTree = &tree;
-                    }
+    MCTree& MCTree::select() {
+        MCTree* bestTree = this;
+        while (!bestTree->children.empty()) {
+            double maxFitness = -INFINITY;
+            for (MCTree* tree : bestTree->children) {
+                const double fitness = tree->selectionFitness();
+                if (fitness > maxFitness) {
+                    maxFitness = fitness;
+                    bestTree = tree;
                 }
             }
-            return *bestTree;
         }
+        return *bestTree;
+    }
 
-        void propagate(const gamestate winner) {
-            Tree* tree = this;
-            while (tree->parent != nullptr) {
-                if (winner / 2 == tree->parent->board.getCurrentPlayer()) {
-                    tree->wins++;
-                }
-                tree->runs++;
-                tree = tree->parent;
+    void MCTree::propagate(const gamestate winner) {
+        MCTree* tree = this;
+        while (tree->parent != nullptr) {
+            if (winner / 2 == tree->parent->board.getCurrentPlayer()) {
+                tree->wins++;
+            }
+            tree->runs++;
+            tree = tree->parent;
+        }
+    }
+
+    void MCTree::clear() {
+        for (MCTree* tree : children) {
+            tree->clear();
+            delete tree;
+        }
+    }
+
+    void MCTree::reroot(MCTree*& root, size_t index) {
+        const size_t n = root->board.getLegalMoves().size();
+        for (size_t i = 0; i < n; ++i) {
+            if (i != index) {
+                root->children[i]->clear();
             }
         }
-    };
+        MCTree* newRoot = root->children[index];
+        newRoot->parent = nullptr;
+        delete root;
+        root = newRoot;
+    }
 
-    MonteCarloTreePlayer::MonteCarloTreePlayer(color playerColor) : MonteCarloPlayer(playerColor, "MTree") {}
+    bool MCTree::findState(std::vector<size_t>& moves, const Board& state, const size_t depth) {
+        if (depth == 0) {
+            return board == state;
+        }
+        const size_t n = board.getLegalMoves().size();
+        for (size_t i = 0; i < n; ++i) {
+            if (children[i]->findState(moves, state, depth-1)) {
+                moves.insert(moves.begin(), i);
+                return true;
+            }
+        }
+        return false;
+    }
 
-    MonteCarloTreePlayer::MonteCarloTreePlayer(color playerColor, const std::string& name) : MonteCarloPlayer(playerColor, name) {}
+    std::vector<size_t> MCTree::findState(const Board& state) {
+        std::vector<size_t> moves;
+        findState(moves, state, state.getPiecesCount() - board.getPiecesCount());
+        return moves;
+    }
 
-    MonteCarloTreePlayer::~MonteCarloTreePlayer() = default;
+
+    MonteCarloTreePlayer::MonteCarloTreePlayer(color playerColor) : MonteCarloPlayer(playerColor, "MTree"), root(nullptr) {}
+
+    MonteCarloTreePlayer::MonteCarloTreePlayer(color playerColor, const std::string& name) : MonteCarloPlayer(playerColor, name), root(nullptr) {}
+
+    MonteCarloTreePlayer::~MonteCarloTreePlayer() {
+        root->clear();
+        delete root;
+    }
 
     const clock_t SAMPLE_TIME = (clock_t)(1 * CLOCKS_PER_SEC);
     Tile MonteCarloTreePlayer::getMoveTimed(const Board& board) {
-        Tree root(board);
+        if (board.getPiecesCount() < lastPiecesCount) {
+            // new game
+            if (root != nullptr) {
+                root->clear();
+                delete root;
+                root = nullptr;
+            }
+        }
+        lastPiecesCount = board.getPiecesCount();
+
+        if (root == nullptr) {
+            // first move
+            root = new MCTree(board);
+        } else {
+            for (const size_t move : root->findState(board)) {
+                MCTree::reroot(root, move);
+            }
+            assert(root->board == board);
+        }
 
         // sample
         const clock_t startTime = clock();
         while (clock() - startTime < SAMPLE_TIME) {
-            Tree& nodeToExpand = root.select();
+            MCTree& nodeToExpand = root->select();
             if (nodeToExpand.board.isGameOver()) {
                 nodeToExpand.propagate(nodeToExpand.board.getGamestate());
             } else {
                 nodeToExpand.expand();
-                Tree& nodeToSimulate = helpers::getRandom(nodeToExpand.children);
+                MCTree& nodeToSimulate = *helpers::getRandom(nodeToExpand.children);
                 const gamestate winner = playRandom(nodeToSimulate.board);
                 nodeToSimulate.propagate(winner);
             }
         }
 
-        // return most sampled
+        // choose most sampled
         const std::vector<Tile>& legalMoves = board.getLegalMoves();
         const size_t n = legalMoves.size();
         size_t mostRuns = 0;
-        Tile bestMove = legalMoves[0];  // default move
+        size_t bestMove = 0;  // default move
         for (size_t i = 0; i < n; ++i) {
-            const size_t runs = root.children[i].runs;
+            const size_t runs = root->children[i]->runs;
             if (runs > mostRuns) {
                 mostRuns = runs;
-                bestMove = legalMoves[i];
+                bestMove = i;
             }
         }
-        return bestMove;
+
+        // reroot according to our chosen move
+        MCTree::reroot(root, bestMove);
+
+        return legalMoves[bestMove];
     }
 }
--- Engine/Ai/MonteCarloTreePlayer.h
+++ Engine/Ai/MonteCarloTreePlayer.h
@@ -5,6 +5,29 @@
 
 namespace reversi {
 
+    class MCTree {
+    public:
+        MCTree* parent;
+        const Board board;
+        size_t wins, runs;
+        std::vector<MCTree*> children;
+
+        MCTree(const Board& board);
+        MCTree(MCTree& parent, const Tile& move);
+
+        void expand();
+        double selectionFitness() const;
+        MCTree& select();
+        void propagate(const gamestate winner);
+        void clear();
+        std::vector<size_t> findState(const Board& state);
+        static void reroot(MCTree*& root, size_t index);
+
+    private:
+        bool findState(std::vector<size_t>& moves, const Board& state, size_t depth);
+    };
+
+
     class MonteCarloTreePlayer : public MonteCarloPlayer {
     public:
         MonteCarloTreePlayer(color playerColor);
@@ -14,6 +37,10 @@ namespace reversi {
         ~MonteCarloTreePlayer() override;
 
         Tile getMoveTimed(const Board& board) override;
+
+    private:
+        MCTree* root;
+        size_t lastPiecesCount;
     };
 
 }
